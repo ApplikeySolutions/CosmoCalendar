2b75f5d190819ab0c556c9ba30067705516cf2c1
Mustafa Bayram <mbayram93@gmail.com>
2018-12-04 14:12:39 +0300

    Current day text color cause a bug and disabled.

--- cosmocalendar/src/main/java/com/applikeysolutions/cosmocalendar/adapter/viewholder/DayHolder.java
+++ cosmocalendar/src/main/java/com/applikeysolutions/cosmocalendar/adapter/viewholder/DayHolder.java
@@ -38,7 +38,7 @@ public class DayHolder extends BaseDayHolder {
 
         if (day.isCurrent()) {
             addCurrentDayIcon(isSelected);
-            ctvDay.setTextColor(calendarView.getCurrentDayTextColor());
+            //ctvDay.setTextColor(calendarView.getCurrentDayTextColor());
         }
 
         if (day.isDisabled()) {
@@ -173,13 +173,7 @@ public class DayHolder extends BaseDayHolder {
         } else if (day.isWeekend()) {
             textColor = calendarView.getWeekendDayTextColor();
             ctvDay.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
-        }
-        else if (day.isCurrent()) {
-            textColor = calendarView.getCurrentDayTextColor();
-            ctvDay.setCompoundDrawablesWithIntrinsicBounds(0, calendarView.getCurrentDayIconRes(), 0, 0);
-        }
-
-        else {
+        } else {
             textColor = calendarView.getDayTextColor();
             ctvDay.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
         }
